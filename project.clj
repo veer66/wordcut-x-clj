5cf1d4e57851dda963e0cd5171a2b28e26022e7b
Vee Satayamas <vsatayamas@gmail.com>
2018-05-08 21:24:09 +0700

    Use Uberjar

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject rockers.veer66/wordcut-x-clj "0.1.0-SNAPSHOT"
+(defproject rockers.veer66/wordcut-x-clj "0.1.1-SNAPSHOT"
   :description "A wordcut-x wrapper for Clojure"
   :url "https://github.com/veer66/wordcut-x-clj"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [rockers.veer66/wordcut-x "1.0-SNAPSHOT"]])
+                 [rockers.veer66/wordcut-x "1.0-SNAPSHOT"]]
+  :profiles {:uberjar {:aot :all}})
   
   
