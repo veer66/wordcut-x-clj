bcd44eb68f036cc33671b658adab40823f5d7ba8
Vee Satayamas <vsatayamas@gmail.com>
2018-05-08 20:56:39 +0700

    Usable code

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject rockers.veer66/wordcut-x-clj "0.1.0-SNAPSHOT"
+  :description "A wordcut-x wrapper for Clojure"
+  :url "https://github.com/veer66/wordcut-x-clj"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [rockers.veer66/wordcut-x "1.0-SNAPSHOT"]])
+  
+  
